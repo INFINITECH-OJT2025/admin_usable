2201636c020d59afb51fb8af807f0806079e09d6
arieljames <arieljamesdeguzman321@gmail.com>
2025-04-30 14:12:44 +0800

    push all frontend

---
+++ next.config.ts
@@ -0,0 +1,28 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+
+
+module.exports = {
+  reactStrictMode: true, // Optional, but recommended
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during build
+  },
+  swcMinify: true, // If you want to enable SWC-based minification (optional)
+  experimental: {
+    react: {
+      throwIfNamespace: false, // Disable the namespace error
+    },
+  },
+  images: {
+    domains: ['127.0.0.1', 'localhost'], // Add any other domains here
+  },
+};
